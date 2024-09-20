25f2de97bc0b4225dc83a91b15970e34fd829f15
Big Josh <otoojoshua616@gmail.com>
2024-09-20 22:30:39 +0000

    DRY Principle

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,24 +6,29 @@ game_dict = {
     'p' : "Paper",
     's' : 'Scissors'
 }
+
 def get_choie():
     try :
         user_input = input("Rock,paper or scissors? (r/p/s) : ").lower()
-        if user_input  in ['r','s','p'] :
+        if user_input  in list(game_dict.keys()) :
             game_options = ["Rock", "Paper", "Scissors"]
             computer_choice = random.choice(game_options).lower()
             user_choice = game_dict[user_input].lower()
             print(f"You chose {user_choice}")
             print(f"Computer Chose {computer_choice}")
+            return user_choice, computer_choice 
         else:
             print("Invalid input")
+            return None, None
     except:
          print("Invalid input") 
-    return user_choice, computer_choice       
+    return None, None
 
 
 def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
+    if user_choice == None:
+        print(' ')
+    elif user_choice == computer_choice and user_choice :
         print("It's a draw game")
     elif (
         (user_choice == "rock" and computer_choice == "paper") or 
